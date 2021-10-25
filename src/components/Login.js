0075f96b52b313ab3e1a082412780ce8df680e7c
alakhber <nakhiyev.alakhber@gmail.com>
2021-10-25 17:15:44 +0400

    get user token  with axios

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
 import { Link } from 'react-router-dom'
 import styles from '../css/Login.module.css'
 import {FcGoogle} from 'react-icons/fc'
@@ -7,14 +8,47 @@ import img from '../img/login.png'
 
 
 function Login() {
+
+ 
+        // fetch('http://127.0.0.1:8000/api/login')
+        // .then(res => res.json())
+        // .then(response => console.log(response))
+    
+    
+   
+    
+    // const [state, setstate] = useState(initialState)
+    const submit = () =>{
+        // const [tokenn, setTokenn] = useState()
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        const article = { email: email, password: password};
+        axios.post('http://127.0.0.1:8000/api/login', article)
+        .then(response => 
+        axios.get('http://127.0.0.1:8000/api/user', { headers: { Authorization: `Bearer ${response.data.token}` }})
+        .then(res => console.log(res))
+        )
+        // .then(response => response.data.succses == true ? setToken(response.data.succses) : null);
+        
+    }
+
+    
+
+        // useEffect(() => {
+        //     axios.get('http://127.0.0.1:8000/api/user')
+        //     .then(response =>console.log(response));
+        // }, [])
+
+    
+   
     return (
         <div className={styles.login}>
             <div className={styles.loginInner}>
                 <div className={styles.left}>
                     <h2>Daxil ol</h2>
-                    <input required type='text' placeholder='İstifadəçi adı və ya e-poçt...'/>
-                    <input required type='text' placeholder='Şifrə...'/>
-                    <button>Daxil ol</button>
+                    <input id='email' required type='text' placeholder='İstifadəçi adı və ya e-poçt...'/>
+                    <input id='password' required type='password' placeholder='Şifrə...'/>
+                    <button onClick={()=>submit()}>Daxil ol</button>
                     <p>və ya</p>
                     <div>
                         <span><a href='https://www.google.com'><FcGoogle/></a></span>
