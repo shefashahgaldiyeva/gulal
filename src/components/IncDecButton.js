51a8a8f2e305548faf98e5466ec1ee6b6adff0b7
shefashahgaldiyeva <83983156+shefashahgaldiyeva@users.noreply.github.com>
2021-12-03 12:28:59 +0400

    Increment and Decrement Button refactored

--- src/components/IncDecButton.js
+++ src/components/IncDecButton.js
@@ -1,41 +1,48 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadIncrementAsync } from '../redux/reducers/products/products.thunks'
-import { loadDecrementAsync } from '../redux/reducers/products/products.thunks'
+import {
+    loadIncrementAsync,
+    loadQuantityAsync,
+    loadDecrementAsync
+  } from "../redux/reducers/products/products.thunks";
 import { getCartAsync } from '../redux/reducers/products/products.thunks'
 
 function IncDecButton(props) {
 
-    const dispatch = useDispatch()
-    const {getingCartProduct, getCartProduct, getCardErrorMessage} = useSelector(state => state.getCartReducer)
-    console.log(getCartProduct)
-      
-    if(getCartProduct){
-        console.log(getCartProduct.data)
-    }
+    const dispatch = useDispatch();
+    const { getingQuantity, getDetail, getQuantityErrorMessage } = useSelector(
+      (state) => state.quantityReducer
+    );
+    useEffect(() => {
+      dispatch(loadQuantityAsync());
+    }, [props.id]);
+    const handlePlus = (id) => {
+      dispatch(
+        loadIncrementAsync({
+          pid: id,
+        })
+      );
+       dispatch(loadQuantityAsync());
+    };
+    const handleMinus = (id) => {
+      dispatch(
+        loadDecrementAsync({
+          pid: id,
+        })
+      );
+      dispatch(loadQuantityAsync());
+    };
 
-    const handlePlus = (id) =>{
-        dispatch(loadIncrementAsync({
-            'pid': id
-        }))
-        dispatch(getCartAsync())
-    }
-    const handleMinus = (id) =>{
-        dispatch(loadDecrementAsync({
-            'pid': id
-        }))
-        dispatch(getCartAsync())
-    }
-    return (
-            <div>
-                <button onClick={() => handleMinus(props.id)}>
-                    -
-                </button>{props.quantity && props.quantity}
-                <button onClick={() => handlePlus(props.id)}>
-                    +
-                </button>
-            </div>
-    )
-    }
+return (
+    <div>
+        <button onClick={() => handleMinus(props.id)}>-</button>
+        {!getingQuantity &&
+            getDetail.data.map(item => item.product_id == props.id ? 
+               item.quantity : null
+            )}
+          <button onClick={() => handlePlus(props.id)}>+</button>
+    </div>
+  );
+}
 
 export default IncDecButton;
