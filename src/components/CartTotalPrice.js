f3bca16b1c85565be616b8a078c777c205f8d7a2
shefashahgaldiyeva <83983156+shefashahgaldiyeva@users.noreply.github.com>
2021-12-03 12:30:00 +0400

    Total Price created

---
+++ src/components/CartTotalPrice.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  loadIncrementAsync,
+  loadQuantityAsync,
+} from "../redux/reducers/products/products.thunks";
+import { loadDecrementAsync } from "../redux/reducers/products/products.thunks";
+import { getCartAsync } from "../redux/reducers/products/products.thunks";
+import styles from "../css/ProductInCart.module.css";
+
+function CartTotalPrice(props) {
+    const dispatch = useDispatch();
+
+    const { getingQuantity, getDetail, getQuantityErrorMessage } = useSelector(
+        (state) => state.quantityReducer
+    );
+    useEffect(() => {
+        dispatch(loadQuantityAsync());
+    }, [props.id]);
+
+    return (
+        <div>
+            {!getingQuantity &&
+                getDetail.data.map(item => item.product_id==props.id? 
+                    <p className={styles.total}>{item.totalPrice} azn</p> : null
+                )}
+        </div>
+    );
+}
+
+export default CartTotalPrice;
