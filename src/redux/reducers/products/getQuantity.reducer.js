37a6559c5536a2d3a2d433bce2980eb49ab6aab4
shefashahgaldiyeva <83983156+shefashahgaldiyeva@users.noreply.github.com>
2021-12-03 12:30:38 +0400

    quantity reducer created

---
+++ src/redux/reducers/products/getQuantity.reducer.js
@@ -0,0 +1,33 @@
+import actionTypes from "./products.actionTypes";
+import initialState from "./quantity.initialState";
+
+const getQuantityReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+			case actionTypes.QUANTITY_LOAD_START:
+				return {
+					...state,
+					getingQuantity: true,
+					getDetail: payload,
+					getQuantityErrorMessage: null,
+				};
+
+			case actionTypes.QUANTITY_LOAD_SUCCESS:
+				return {
+					...state,
+					getingQuantity: false,
+					getDetail: payload,
+				};
+
+			case actionTypes.QUANTITY_LOAD_ERROR:
+				return {
+					...state,
+					getingQuantity: false,
+					getQuantityErrorMessage: payload,
+				};			
+
+		default:
+			return state;
+	}
+};
+
+export default getQuantityReducer;
